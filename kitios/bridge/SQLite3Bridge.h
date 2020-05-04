e61805bf17b6ecb60602286c88054fd1e605e9d2
Graeme Costin <graeme_costin@wycliffe.org.au>
2020-05-04 15:53:25 +1000

    Added bridge to SQLite library

---
+++ kitios/bridge/SQLite3Bridge.h
@@ -0,0 +1,14 @@
+//
+//  SQLite3Bridge.h
+//  KIT02
+//
+//  Created by Graeme Costin on 16/9/19.
+//  Copyright © 2019 Costin Computing Services. All rights reserved.
+//
+
+#ifndef SQLite3Bridge_h
+#define SQLite3Bridge_h
+
+#import <sqlite3.h>
+
+#endif /* SQLite3Bridge_h */
