36f542f04835d7fe2ef4ee8ac4b09638c8f8d3d3
Graeme Costin <graeme_costin@wycliffe.org.au>
2020-05-04 14:53:12 +1000

    Copied across the contents of AppDelegate.swift

--- kitios/kitios/AppDelegate.swift
+++ kitios/kitios/AppDelegate.swift
@@ -11,27 +11,82 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+	var window: UIWindow?
+	// A pointer to the one and only KITDAO instance is kept in the AppDelegate
+	// so that all parts of the app can use it
+	var dao: KITDAO?
+		
+	public var bibInst: Bible?			// During the launch of KIT an instance of the class Bible will be created
+	public var bookInst: Book?			// Once launching is complete there will be an instance of the current Book
+	public var chapInst: Chapter?		// Once launching is complete there will be an instance of the current Chapter
+	public var VTVCtrl: VersesTableViewController?	// Once a Chapter of a Book is opened there will be a VersesTableViewController
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+
+		do {
+			let srcPath:URL = Bundle.main.url (forResource: "kdb", withExtension: "sqlite")!
+			let docsDir:URL = FileManager.default.urls (for: .documentDirectory, in: .userDomainMask).first!
+			let destPath:URL = docsDir.appendingPathComponent ("kdb.sqlite")
+			if !FileManager.default.fileExists(atPath: destPath.path) {
+				try! FileManager.default.copyItem(atPath: srcPath.path, toPath: destPath.path)
+				print("AppDelegate kdb.sqlite copied to Documents directory")
+			} else {
+				print("AppDelegate kdb.sqlite already in Documents directory")
+			}
+		}
+		// the small file kdb.sqlite is shipped with the program so there will be no error thrown
+		// by the FileManager copyItem function
+		// catch {
+		//print("kdb.sqlite unknown error. Error No. \(error)")
+		//}
+
+		dao = KITDAO()	// create an instance of the Data Access Object and keep reference to it
+
 		return true
 	}
-
-	// MARK: UISceneSession Lifecycle
-
-	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-		// Called when a new scene session is being created.
-		// Use this method to select a configuration to create the new scene with.
-		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+	
+	func applicationWillResignActive(_ application: UIApplication) {
+		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+		print("AppDelegate KIT will resign from Active")
+		// Save the current VerseItem if necessary
+		if VTVCtrl != nil {
+			VTVCtrl!.saveCurrentItemText()
+		}
 	}
-
-	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-		// Called when the user discards a scene session.
-		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+	
+	func applicationDidEnterBackground(_ application: UIApplication) {
+		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+		print("AppDelegate KIT has entered background")
+		// KIT does not do background execution so nothing to do here.
+	}
+	
+	func applicationWillEnterForeground(_ application: UIApplication) {
+		// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+		
+		print("AppDelegate KIT about to enter foreground")
+		// KIT does not enter background execution so nothing to do here.
+	}
+	
+	func applicationDidBecomeActive(_ application: UIApplication) {
+		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+		
+		print("AppDelegate KIT became active")
+		// KIT should refresh its user interface when returning to foreground??
+//		bibInst!.refreshUIAfterReturnToForeground()
+		
+	}
+	
+	func applicationWillTerminate(_ application: UIApplication) {
+		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+		
+		print("AppDelegate KIT will now terminate")
+		// KIT needs to delete its instance of the class Bible and all the instances that are owned by it,
+		// including the instance of KITDAO (which closes the kdb.sqlite database)
+		bibInst = nil
 	}
 
 
 }
-
