f07f59586001dd72f6e93f06b930e56178de22f5
Graeme Costin <graeme_costin@wycliffe.org.au>
2020-05-04 14:21:13 +1000

    Initial step in creating of a new project folder for kitios

---
+++ kitios/kitios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kitios
+//
+//  Created by Graeme Costin on 4/5/20.
+//  Copyright © 2020 Costin Computing Services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
